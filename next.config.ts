194dce35cc3821901523f8622eddf16cfd79fef2
Srinivasu <srinivasuvendra2006@gmail.com>
2025-09-21 09:57:10 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  fetchCache: 'only-no-store',
   typescript: {
     ignoreBuildErrors: true,
   },
