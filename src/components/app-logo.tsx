f73041431678f1f2ea367275dad5ca1a495e7f10
Srinivasu <srinivasuvendra2006@gmail.com>
2025-09-22 09:41:55 +0000

    in light mode make the app name into black colour

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -5,10 +5,10 @@ export function AppLogo() {
     <div className="flex flex-col items-center gap-2">
         <Logo className="h-28 w-28" />
         <div className='text-center'>
-            <span className="text-5xl font-bold tracking-widest text-white">
+            <span className="text-5xl font-bold tracking-widest text-black dark:text-white">
                 TRIP MATE
             </span>
-            <p className='mt-2 text-lg font-medium tracking-wider text-blue-200'>MAKE YOUR TRIP MEMORABLE</p>
+            <p className='mt-2 text-lg font-medium tracking-wider text-blue-800 dark:text-blue-200'>MAKE YOUR TRIP MEMORABLE</p>
         </div>
     </div>
   );
